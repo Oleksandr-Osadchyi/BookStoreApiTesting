0eb8ce308c08289a6d4737b975316068af362d42
Oleksandr-Osadchyi <oleksandr.osadchyi@avenga.com>
2026-03-26 17:40:14 +0200

    uncomment users

--- src/main/java/api/users/User.java
+++ src/main/java/api/users/User.java
@@ -1,32 +1,32 @@
-//package api.users;
-//
-//import lombok.*;
-//import utils.FakerUtil;
-//
-//
-//@Getter
-//@Setter
-//@Builder
-//@NoArgsConstructor
-//@AllArgsConstructor
-//public class User {
-//    private int id;
-//    private String userName;
-//    private String password;
-//
-//    public User createUserModel(int id, String userName, String password) {
-//        return User.builder()
-//                .id(id)
-//                .userName(userName)
-//                .password(password)
-//                .build();
-//    };
-//
-//    public User createRandomUserModel() {
-//        return User.builder()
-//                .id(FakerUtil.randomNumber(1000, 100000))
-//                .userName(FakerUtil.randomEmailAddress())
-//                .password(FakerUtil.randomPassword())
-//                .build();
-//    }
-//}
+package api.users;
+
+import lombok.*;
+import utils.FakerUtil;
+
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String userName;
+    private String password;
+
+    public User createUserModel(int id, String userName, String password) {
+        return User.builder()
+                .id(id)
+                .userName(userName)
+                .password(password)
+                .build();
+    };
+
+    public User createRandomUserModel() {
+        return User.builder()
+                .id(FakerUtil.randomNumber(1000, 100000))
+                .userName(FakerUtil.randomEmailAddress())
+                .password(FakerUtil.randomPassword())
+                .build();
+    }
+}
--- src/main/java/api/users/UsersClient.java
+++ src/main/java/api/users/UsersClient.java
@@ -1,45 +1,45 @@
-//package api.users;
-//
-//import api.engine.BookStoreObject;
-//import api.engine.BookStoreObjectApiClient;
-//import api.engine.ResponseSpecifications;
-//
-//import java.util.List;
-//
-//public class UsersClient {
-//
-//    private static final BookStoreObjectApiClient bookStoreObjectApiService = new BookStoreObjectApiClient(BookStoreObject.USERS);
-//
-//    public User createUser(User user) {
-//        return bookStoreObjectApiService
-//                .postObject(user)
-//                .validate(ResponseSpecifications.responseCode200Spec())
-//                .getRecord(User.class);
-//    }
-//
-//    public List<User> getUsers() {
-//        return bookStoreObjectApiService
-//                .getObjects()
-//                .validate(ResponseSpecifications.responseCode200Spec())
-//                .getRecords(User.class);
-//    }
-//
-//    public User getUser(int userId) {
-//        return bookStoreObjectApiService
-//                .getObject(userId)
-//                .validate(ResponseSpecifications.responseCode200Spec())
-//                .getRecord(User.class);
-//    }
-//
-//    public void updateUser(int userId, User user) {
-//        bookStoreObjectApiService
-//                .updateObject(userId, user)
-//                .validate(ResponseSpecifications.responseCode200Spec());
-//    }
-//
-//    public void deleteUser(int userId) {
-//        bookStoreObjectApiService
-//                .deleteObject(userId)
-//                .validate(ResponseSpecifications.responseCode200Spec());
-//    }
-//}
+package api.users;
+
+import api.engine.BookStoreObject;
+import api.engine.BookStoreObjectApiClient;
+import api.engine.ResponseSpecifications;
+
+import java.util.List;
+
+public class UsersClient {
+
+    private static final BookStoreObjectApiClient bookStoreObjectApiService = new BookStoreObjectApiClient(BookStoreObject.USERS);
+
+    public User createUser(User user) {
+        return bookStoreObjectApiService
+                .postObject(user)
+                .validate(ResponseSpecifications.responseCode200Spec())
+                .getRecord(User.class);
+    }
+
+    public List<User> getUsers() {
+        return bookStoreObjectApiService
+                .getObjects()
+                .validate(ResponseSpecifications.responseCode200Spec())
+                .getRecords(User.class);
+    }
+
+    public User getUser(int userId) {
+        return bookStoreObjectApiService
+                .getObject(userId)
+                .validate(ResponseSpecifications.responseCode200Spec())
+                .getRecord(User.class);
+    }
+
+    public void updateUser(int userId, User user) {
+        bookStoreObjectApiService
+                .updateObject(userId, user)
+                .validate(ResponseSpecifications.responseCode200Spec());
+    }
+
+    public void deleteUser(int userId) {
+        bookStoreObjectApiService
+                .deleteObject(userId)
+                .validate(ResponseSpecifications.responseCode200Spec());
+    }
+}
